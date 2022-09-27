5242967c8f4663344bcaa75a5ff37a65505e4f12
RaiAyub <47720645+RaiAyub@users.noreply.github.com>
2022-09-27 09:39:21 -0400

    String message change

--- cloud-rest-api/src/main/java/com/amazon/web/cloudrestapi/service/MathService.java
+++ cloud-rest-api/src/main/java/com/amazon/web/cloudrestapi/service/MathService.java
@@ -8,7 +8,7 @@ public class MathService {
     public Result add(int a, int b){
         Result result = new Result();
         int sum=a+b;
-        String message = "Sum is: "+sum;
+        String message = "Sum of : " +a+ " & "+b+ " is " +sum;
         result.setMathResult(sum);
         result.setMessage(message);
         return result;
